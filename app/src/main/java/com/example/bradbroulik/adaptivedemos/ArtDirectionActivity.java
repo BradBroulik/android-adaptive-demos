7e82966eedf78b2f85473bba1d8c80e986bb95fe
BradBroulik <BradBroulik@gmail.com>
2015-03-08 17:21:16 -0500

    Added art direction example

---
+++ app/src/main/java/com/example/bradbroulik/adaptivedemos/ArtDirectionActivity.java
@@ -0,0 +1,15 @@
+package com.example.bradbroulik.adaptivedemos;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class ArtDirectionActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_direction);
+    }
+
+}
